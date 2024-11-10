bf5a0d8b63ddcd37c050278681ebe724b32da521
ElenMuradyan <muradyanelen0701@gmail.com>
2024-11-10 17:04:40 +0400

    fix: fix the way of taking firstname from userProfileInfo

--- src/pages/GameStart/index.js
+++ src/pages/GameStart/index.js
@@ -37,12 +37,12 @@ const GameStart = () => {
             <Flex align="center" justify="center" vertical>
                 <Title style={{ color: 'white' }}>{language === 'en' ? 'Start The Game' : 'Սկսել Խաղը'}</Title>
                 <Title level={5} style={{ color: 'white' }}>
-                {language === 'en' ? `Hi ${userProfileInfo.firstName}. Step into the world of high-stakes trivia with the Millionaire Quiz Game! 
+                {language === 'en' ? `Hi ${userProfileInfo.firstName || ''}. Step into the world of high-stakes trivia with the Millionaire Quiz Game! 
                     Test your knowledge across various categories, answer challenging questions, and climb the ladder 
                     to win virtual millions. With lifelines at your disposal, including Phone a Friend and Ask the Audience, 
                     strategize your way to the top. Are you ready to become a quiz millionaire? 
                     Play now and see how far your knowledge can take you!` : 
-                     `Բարև ${userProfileInfo.firstName} Միլիոնատեր Վիկտորինայի խաղում! Տվեք մի քանի հարցերի ճիշտ պատասխաններ և հաղթեք վիրտուալ միլիոններ: Կկարողանաք հաղթել և դառնալ միլիոնատեր? Աշխատեք հիմա և բացահայտեք, թե որքան հեռու կարող եք հասնել ձեր գիտելիքներով:`}
+                     `Բարև ${userProfileInfo.firstName || ''} Օնլայն Միլիոնատեր Վիկտորինայի խաղում! Տվեք մի քանի հարցերի ճիշտ պատասխաններ և հաղթեք վիրտուալ միլիոններ: Կկարողանաք հաղթել և դառնալ միլիոնատեր? Աշխատեք հիմա և բացահայտեք, թե որքան հեռու կարող եք հասնել ձեր գիտելիքներով:`}
                     
                 </Title>
                 <Button type="primary" size="large" onClick={handleStartGame}>
